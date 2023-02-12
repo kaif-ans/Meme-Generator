cc8f34da13935ccc73d4203bead378a46ad569af
kaif-ans <kaifansari989@gmail.com>
2023-02-12 22:14:33 +0530

    not done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Header";
+import Meme from "./Meme";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
